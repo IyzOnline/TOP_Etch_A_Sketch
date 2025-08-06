47ef88e80f7be0b15aa8acd31c1b32883b6f5873
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-08-06 15:26:37 +0800

    feat: Make random color generator through hexcode color generator

--- index.js
+++ index.js
@@ -41,3 +41,15 @@ function tableCreation(dimensions){
         container.appendChild(row);
     }
 }
+
+function randomizeColor(){
+    let finalColor = "#";
+    for(let i = 0; i < 6; i++){
+        let result = Math.floor((Math.random * 16) + 1);
+        if(result >= 10){
+            result = String.fromCharCode(result + 55);
+        }
+        finalColor += result;
+    }
+    return finalColor;
+}
